7976d8ee4a38c19c9998bed0482a4283aff49779
Davide Setti <davide.setti@gmail.com>
2009-07-22 14:14:20 +0200

    sonetgraph module added

--- analysis.py
+++ analysis.py
@@ -1,73 +1,21 @@
 #!/usr/bin/env python
 
-#system
-import igraph as ig
+# system
+
 from time import ctime
 from optparse import OptionParser
 import os, sys, re
 import gc
 import numpy
+import sonetgraph as sg
+import igraph as ig
 
-#project
+# project
 from tablr import Tablr
 
 ## GLOBAL VARIABLES
 
 ## FUNCTIONS
-
-
-
-def averageDistance(g, weighted=True):
-    isinstance(g, ig.Graph) # helper for wing
-    dSum = 0.
-    step = 1000
-    n = len(g.vs)
-    for i in range(0, n, step):
-        ##if not (i+1) % 100:
-        ##    print 'Step:', i
-        ##    print 1.*dSum*step/i
-        uplimit = min(n, i+step)
-
-        # distances from nodes in range (i, i+step) to all the other nodes
-        if weighted:
-            aDistances = g.shortest_paths(range(i, uplimit), weights='length')
-        else:
-            aDistances = g.shortest_paths(range(i, uplimit))
-
-        dSum += 1.*sum([sum(d) for d in aDistances]) / (n-1) / (uplimit - i)
-
-    avg_dist = 1.*dSum / len(range(0, n, step))
-    return avg_dist
-
-
-def efficiency(g):
-    isinstance(g, ig.Graph) # helper for wing
-    effSum = 0.
-    step = 1000
-    n = len(g.vs)
-    for i in range(0, n, step):
-        ##if not (i+1) % 100:
-        ##    print 'Step:', i
-        ##    print 1.*dSum*step/i
-        uplimit = min(n, i+step)
-
-        # distances from nodes in range (i, i+step) to all the other nodes
-        aDistances = g.shortest_paths(range(i, uplimit), weights='length')
-        effSum += 1.*sum(sum(1./numpy.array([e for e in d if e])) for d in aDistances)
-
-    efficiency = effSum/(1.*n*(n-1)) # maybe there should be a factor of 2 somewhere (directed graph)
-    return efficiency
-
-
-def set_weighted_degree(g, type=ig.IN):
-    #todo: improve self-loops check
-    stype = type == ig.IN and "in" or "out"
-    k = 'weighted_%sdegree' % stype
-
-    for node in g.vs:
-        edges = g.adjacent(node.index, type=type)
-        g_edges = (e for e in edges if not g.is_loop(e))
-        node[k] = sum(g.es[eid]['weight'] for eid in g_edges)
 
 
 if __name__ == '__main__':
@@ -103,38 +51,41 @@ if __name__ == '__main__':
     lang = s[:s.index('wiki')]
     res = re.search('wiki-(\d{4})(\d{2})(\d{2})',s)
     date = ''.join([res.group(x) for x in xrange(1,4)])
-    g = ig.load(fn)
-    isinstance(g, ig.Graph) # helper for wing
-    g.es['length'] = 1./numpy.array(g.es['weight'])
+
+    g = sg.load(fn)
+    g.invert_edge_attr('weight', 'length')
+
+    vn = len(g.g.vs) # number of vertexes
+    en = len(g.g.es) # number of edges
 
     if options.as_table:
         tablr = Tablr()
         tablr.start(1024*16, lang)
 
     if options.details:
-        print " * vertexes: %d" % (len(g.vs),)
-        print " * edges: %d" % (len(g.es),)
+        print " * vertexes: %d" % (vn,)
+        print " * edges: %d" % (en,)
 
-        nodes_with_outdegree = len(g.vs.select(_outdegree_ge=1))
-        nodes_with_indegree = len(g.vs.select(_indegree_ge=1))
+        nodes_with_outdegree = len(g.g.vs.select(_outdegree_ge=1))
+        nodes_with_indegree = len(g.g.vs.select(_indegree_ge=1))
 
-        print " * #nodes with out edges: %d (%6f%%)" % (nodes_with_outdegree, 100.*nodes_with_outdegree/len(g.vs))
-        print " * #nodes with in edges: %d (%6f%%)" % (nodes_with_indegree, 100.*nodes_with_indegree/len(g.vs))
-        print " * 5 max numMsg on edges : %s" % (', '.join(str(idx) for idx in sorted(g.es['weight'], reverse=True)[:5]),)
-        print " * reciprocity : %6f" % g.reciprocity()
-        print " * diameter : %6f" % g.diameter(weights='length')
+        print " * #nodes with out edges: %d (%6f%%)" % (nodes_with_outdegree, 100.*nodes_with_outdegree/vn)
+        print " * #nodes with in edges: %d (%6f%%)" % (nodes_with_indegree, 100.*nodes_with_indegree/vn)
+        print " * 5 max numMsg on edges : %s" % (', '.join(str(idx) for idx in sorted(g.g.es['weight'], reverse=True)[:5]),)
+        print " * reciprocity : %6f" % g.g.reciprocity()
+        print " * diameter : %6f" % g.g.diameter(weights='length')
 
-        print " * Average weights : %6f" % numpy.average(g.es['weight'])
+        print " * Average weights : %6f" % numpy.average(g.g.es['weight'])
 
 
     if options.density:
-        print " * density: %.10f" % (g.density(),)
+        print " * density: %.10f" % (g.g.density(),)
 
         #print " * calculated density: %.10f" % (1.*len(g.es)/lenvs/(lenvs-1))
 
     if options.degree:
-        ind = numpy.array(g.degree(type=ig.IN))
-        outd = numpy.array(g.degree(type=ig.OUT))
+        ind = numpy.array(g.g.degree(type=ig.IN))
+        outd = numpy.array(g.g.degree(type=ig.OUT))
         mid = numpy.average(ind)
         mod = numpy.average(outd)
 
@@ -150,46 +101,47 @@ if __name__ == '__main__':
         pass
 
     if options.summary:
-        print " * summary: %s" % (g.summary(), )
+        print " * summary: %s" % (g.g.summary(), )
 
     if options.distance:
-        vc = g.clusters()
+        vc = g.g.clusters()
         max_clusters = sorted(vc.sizes(), reverse=True)[:5]
         giant = vc.giant()
 
         print " * length 5 max clusters: %s" % ', '. join(map(str, max_clusters))
-        print " * #node in 5 max clusters/#all nodes: %s" % ', '.join("%6f" % (1.*cluster_len/len(g.vs),) for cluster_len in max_clusters)
+        print " * #node in 5 max clusters/#all nodes: %s" % ', '.join("%6f" % (1.*cluster_len/vn,) for cluster_len in max_clusters)
 
     if options.distance:
-        print " * average distance in the giant component: %f" % averageDistance(giant)
-        print " * average hops in the giant component: %f" % averageDistance(giant, False)
+        gg = sg.Graph(giant)
+        print " * average distance in the giant component: %f" % gg.averageDistance(weight='length')
+        print " * average hops in the giant component: %f" % gg.averageDistance()
 
         #print "Average distance 2: %f" % giant.average_path_length(True, False)
 
 
     if options.efficiency:
-        print " * efficiency: %f" % efficiency(g)
+        print " * efficiency: %f" % g.efficiency(weight='length')
 
 
     if options.plot or options.histogram or options.power_law or options.centrality:
-        set_weighted_degree(g)
+        g.set_weighted_degree()
 
     if options.centrality:
-        g.vs['bw'] = g.betweenness(weights='length', directed = True)
-        #g.vs['ev'] = g.evcent(weights='weight') # eigenvector centrality
-        g.vs['pr'] = g.pagerank(weights='weight') # pagerank
-        set_weighted_degree(g, type=ig.OUT)
-        total_weights = sum(g.es['weight'])
-        max_edges = len(g.vs)*(len(g.vs)-1)
+        g.g.vs['bw'] = g.g.betweenness(weights='length', directed = True)
+        #g.g.vs['ev'] = g.g.evcent(weights='weight') # eigenvector centrality
+        g.g.vs['pr'] = g.g.pagerank(weights='weight') # pagerank
+        g.set_weighted_degree(type=ig.OUT)
+        total_weights = sum(g.g.es['weight'])
+        max_edges = vn*(vn-1)
 
-        print " * Average betweenness : %6f" % (numpy.average(g.vs['bw'])/max_edges,)
+        print " * Average betweenness : %6f" % (numpy.average(g.g.vs['bw'])/max_edges,)
         #print " * Average eigenvector centrality : %6f" % numpy.average(g.vs['ev'])
-        print " * Average pagerank : %6f" % numpy.average(g.vs['pr'])
-        print " * Average IN degree centrality (weighted): %6f" % numpy.average(g.vs['weighted_indegree'])
-        print " * Average OUT degree centrality (weighted) : %6f" % numpy.average(g.vs['weighted_outdegree'])
+        print " * Average pagerank : %6f" % numpy.average(g.g.vs['pr'])
+        print " * Average IN degree centrality (weighted): %6f" % numpy.average(g.g.vs['weighted_indegree'])
+        print " * Average OUT degree centrality (weighted) : %6f" % numpy.average(g.g.vs['weighted_outdegree'])
 
     if options.power_law:
-        indegrees = g.vs['weighted_indegree']
+        indegrees = g.g.vs['weighted_indegree']
 
         alpha_exp = ig.statistics.power_law_fit(indegrees, xmin=1)
 
@@ -197,22 +149,22 @@ if __name__ == '__main__':
 
     if options.histogram:
         # group
-        nogrp_indegrees = g.vs.select(sysop_ne=True, bureaucrat_ne=True, steward_ne=True, founder_ne=True, bot_ne=True)['indegree']
+        nogrp_indegrees = g.g.vs.select(sysop_ne=True, bureaucrat_ne=True, steward_ne=True, founder_ne=True, bot_ne=True)['indegree']
         nogrp_list = [(degree, 1) for degree in nogrp_indegrees if degree]
 
-        sysops_indegrees = g.vs.select(sysop=True, bureaucrat_ne=True, steward_ne=True, founder_ne=True, bot_ne=True)['indegree']
+        sysops_indegrees = g.g.vs.select(sysop=True, bureaucrat_ne=True, steward_ne=True, founder_ne=True, bot_ne=True)['indegree']
         sysops_list = [(degree, 2) for degree in sysops_indegrees if degree]
 
-        burs_indegrees = g.vs.select(bureaucrat=True, steward_ne=True, founder_ne=True, bot_ne=True)['indegree']
+        burs_indegrees = g.g.vs.select(bureaucrat=True, steward_ne=True, founder_ne=True, bot_ne=True)['indegree']
         burs_list = [(degree, 3) for degree in burs_indegrees if degree]
 
-        stewards_indegrees = g.vs.select(steward=True, founder_ne=True, bot_ne=True)['indegree']
+        stewards_indegrees = g.g.vs.select(steward=True, founder_ne=True, bot_ne=True)['indegree']
         stewards_list = [(degree, 4) for degree in stewards_indegrees if degree]
 
-        founders_indegrees = g.vs.select(founder=True, bot_ne=True)['indegree']
+        founders_indegrees = g.g.vs.select(founder=True, bot_ne=True)['indegree']
         founders_list = [(degree, 5) for degree in founders_indegrees if degree]
 
-        bots_indegrees = g.vs.select(bot=True)['indegree']
+        bots_indegrees = g.g.vs.select(bot=True)['indegree']
         bots_list = [(degree, 6) for degree in bots_indegrees if degree]
 
         if options.gnuplot:
@@ -243,28 +195,28 @@ if __name__ == '__main__':
 
     if options.plot:
         import math
-        bots = g.vs.select(bot=True)
+        bots = g.g.vs.select(bot=True)
         bots['color'] = ('purple',)*len(bots)
 
-        burs = g.vs.select(bureaucrat=True)
+        burs = g.g.vs.select(bureaucrat=True)
         burs['color'] = ('blue',)*len(burs)
 
-        sysops = g.vs.select(sysop=True)
+        sysops = g.g.vs.select(sysop=True)
         sysops['color'] = ('yellow',)*len(sysops)
 
-        bur_sysops = g.vs.select(bureaucrat=True, sysop=True)
+        bur_sysops = g.g.vs.select(bureaucrat=True, sysop=True)
         bur_sysops['color'] = ('orange',)*len(bur_sysops)
 
-        g.vs['size'] = [math.sqrt(v['indegree']+1)*10 for v in g.vs]
+        g.g.vs['size'] = [math.sqrt(v['indegree']+1)*10 for v in g.g.vs]
 
-        ig.plot(g, target=lang+"_general.png", bbox=(0,0,4000,2400), edge_color='grey', layout='fr')
-        weights = g.es['weight']
+        ig.plot(g.g, target=lang+"_general.png", bbox=(0,0,4000,2400), edge_color='grey', layout='fr')
+        weights = g.g.es['weight']
         max_weight = max(weights)
 
-        g.es['color'] = [(255.*e['weight']/max_weight, 0., 0.) for e in g.es]
-        g.es['width'] = weights
+        g.g.es['color'] = [(255.*e['weight']/max_weight, 0., 0.) for e in g.g.es]
+        g.g.es['width'] = weights
 
-        ig.plot(g, target=lang+"_weighted_edges.png", bbox=(0,0,4000,2400), layout='fr', vertex_label=' ')
+        ig.plot(g.g, target=lang+"_weighted_edges.png", bbox=(0,0,4000,2400), layout='fr', vertex_label=' ')
 
 
     if options.as_table:
---
+++ sonetgraph.py
@@ -0,0 +1,81 @@
+import igraph as ig
+import numpy
+
+def load(fn):
+    return Graph(ig.load(fn))
+
+class Graph(object):
+    #g = attr(ig.Graph)
+
+    def __init__(self, g):
+        self.g = g
+
+    def invert_edge_attr(self, source, dest):
+        self.g.es[dest] = 1./numpy.array(self.g.es[source])
+
+
+    def efficiency(self, weight=None):
+        r"""Returns the efficiency of the graph
+
+        @param weight: (string) specify which attribute to use. Do not specify if the graph is not weighted
+        """
+        isinstance(self.g, ig.Graph) # helper for wing
+        effSum = 0.
+        step = 1000
+        n = len(self.g.vs)
+        for i in range(0, n, step):
+            ##if not (i+1) % 100:
+            ##    print 'Step:', i
+            ##    print 1.*dSum*step/i
+            uplimit = min(n, i+step)
+
+            # distances from nodes in range (i, i+step) to all the other nodes
+            if weight:
+                aDistances = self.g.shortest_paths(range(i, uplimit), weights=weight)
+            else:
+                aDistances = self.g.shortest_paths(range(i, uplimit))
+
+            effSum += 1.*sum(sum(1./numpy.array([e for e in d if e])) for d in aDistances)
+
+        efficiency = effSum/(1.*n*(n-1)) # maybe there should be a factor of 2 somewhere (directed graph)
+        return efficiency
+
+
+    def set_weighted_degree(self, type=ig.IN):
+        #todo: improve self-loops check
+        stype = type == ig.IN and "in" or "out"
+        k = 'weighted_%sdegree' % stype
+
+        for node in self.g.vs:
+            edges = self.g.adjacent(node.index, type=type)
+            g_edges = (e for e in edges if not self.g.is_loop(e))
+            node[k] = sum(self.g.es[eid]['weight'] for eid in g_edges)
+
+
+
+    def averageDistance(self, weight=None):
+        r"""Returns the average shortest path length of the graph
+
+        @param weight: (string) specify which attribute to use. Do not specify if the graph is not weighted
+        """
+        isinstance(self.g, ig.Graph) # helper for wing
+        dSum = 0.
+        step = 1000
+        n = len(self.g.vs)
+        for i in range(0, n, step):
+            ##if not (i+1) % 100:
+            ##    print 'Step:', i
+            ##    print 1.*dSum*step/i
+            uplimit = min(n, i+step)
+
+            # distances from nodes in range (i, i+step) to all the other nodes
+            if weight:
+                aDistances = self.g.shortest_paths(range(i, uplimit), weights=weight)
+            else:
+                aDistances = self.g.shortest_paths(range(i, uplimit))
+
+            dSum += 1.*sum([sum(d) for d in aDistances]) / (n-1) / (uplimit - i)
+
+        avg_dist = 1.*dSum / len(range(0, n, step))
+        return avg_dist
+
