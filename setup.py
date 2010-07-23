54835543dc946d3537dccb3bd5a4dd5b17768f78
Davide Setti <davide.setti@gmail.com>
2010-07-23 16:08:06 +0200

    adding setup.py with requirements

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+#from distutils.extension import Extension
+#from Cython.Distutils import build_ext
+
+#ext_modules = [Extension("cmwlib", ["cmwlib.pyx"])]
+#ext_modules = [Extension("cedgecache", ["cedgecache.pyx"])]
+
+setup(
+    name = "wiki-network",
+    description = 'Wikipedia Social Network Analysis',
+    version = "0.1",
+    install_requires = ('lxml', 'celery', 'django-celery', 'django_evolution',
+                        'django', 'django_extensions')
+    #cmdclass = {'build_ext': build_ext},
+    #ext_modules = ext_modules
+)
