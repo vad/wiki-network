9aba08077f31ea5c58954978ce3a667d624c101a
Davide Setti <davide.setti@gmail.com>
2010-08-05 12:04:15 +0200

    better admin for events

--- django_wikinetwork/wikinetwork/admin.py
+++ django_wikinetwork/wikinetwork/admin.py
@@ -2,6 +2,21 @@ from django_wikinetwork.wikinetwork.models import WikiRunData, \
      WikiRunGroupData, WikiStat, WikiLang, BigWikiStat, CeleryRun, \
      WikiEvent
 from django.contrib import admin
+from django.forms import Textarea
+from datetime import date, timedelta
+
+class DictTimeField(Textarea):
+    def render(self, name, value, attrs=None):
+        if attrs is None: attrs = {}
+        attrs['readonly'] = 'readonly'
+        if isinstance(value, dict):
+            d = {}
+            for k, v in value.iteritems():
+                da = date(2001, 1, 1) + timedelta(k)
+                d['%s-%s-%s' % (da.year, da.month, da.day)] = v
+            value = d
+
+        return super(DictTimeField, self).render(name, value, attrs)
 
 class WikiRunDataAdmin(admin.ModelAdmin):
     list_display    = ('lang', 'date', 'created')
@@ -19,9 +34,18 @@ class WikiStatAdmin(admin.ModelAdmin):
     date_hierarchy  = 'created'
 
 class WikiEventAdmin(admin.ModelAdmin):
+    fields          = ('lang', 'title', 'normal', 'talk', 'desired')
     list_display    = ('lang', 'title')
     list_filter     = ('lang',)
     search_fields   = ('title',)
+    readonly_fields = ('lang', 'title', 'desired')
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name in ('talk', 'normal'):
+            kwargs['widget'] = DictTimeField
+
+        return super(WikiEventAdmin, self
+                     ).formfield_for_dbfield(db_field, **kwargs)
 
 admin.site.register(WikiRunData, WikiRunDataAdmin)
 admin.site.register(WikiRunGroupData, WikiRunGroupDataAdmin)
